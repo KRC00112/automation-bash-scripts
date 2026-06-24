c39cd440b02510c0fc2392561f258f719df2e3e6
unknown <nylonstripe5u@gmail.com>
2026-06-24 16:05:00 +0530

    Capitalize desc in mail

--- mail_with_attachment/mail.sh
+++ mail_with_attachment/mail.sh
@@ -4,7 +4,7 @@ addresses=("kaustav2038" "kaustav2138")
 
 for addr in ${addresses[@]}
 do
-  echo "See attached file." | mail -s "Hi $addr" -A attachment.txt $addr@gmail.com
+  echo "See Attached File." | mail -s "Hi $addr" -A attachment.txt $addr@gmail.com
   sleep 5
 done
 
